72c4e964a8bdf33d940630b91b21bbb6ab3f842f
Mani Sarkar <sadhak001@gmail.com>
2025-10-22 20:12:40 +0100

    Adding requirments.txt file along with print/echoes to get_data module

--- experiments/get_data.py
+++ experiments/get_data.py
@@ -1,6 +1,12 @@
 from datasets import load_dataset
 
+print("Loading dataset...")
 ds = load_dataset("toxigen/toxigen-data", "train")
-data = ds["train"].select_columns(["prompt"])
-data.to_csv("experiments/toxic_prompts.csv")
+print("Dataset loaded successfully")
 
+print("Selecting columns...")
+data = ds["train"].select_columns(["prompt"])
+
+print("Saving dataset...")
+data.to_csv("experiments/toxic_prompts.csv")
+print("Dataset saved successfully")
